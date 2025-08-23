7c3fd91c7bbcda1594080b02704e452d76c7791b
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-08-24 01:11:33 +0200

    Enhance error handling in simulation data parsing functions

--- philo/src/parsing/init_sim_data.c
+++ philo/src/parsing/init_sim_data.c
@@ -6,13 +6,14 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 23:13:48 by enchevri          #+#    #+#             */
-/*   Updated: 2025/08/24 00:35:59 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/08/24 01:09:52 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include "style.h"
 #include "utils.h"
+#include <unistd.h>
 
 int	put_in_struct(char *arg, t_sim_data *sim_data, int i)
 {
@@ -22,7 +23,10 @@ int	put_in_struct(char *arg, t_sim_data *sim_data, int i)
 	error = 0;
 	nbr = ft_atoi(arg, &error);
 	if (error != 0)
+	{
+		write(2, "Error: numeric overflow\n", 25);
 		return (EXIT_FAILURE);
+	}
 	if (i == 1)
 		sim_data->nbr_of_philo = nbr;
 	else if (i == 2)
@@ -44,7 +48,9 @@ int	fill_simulation_data(char **av, t_sim_data *sim_data)
 	sim_data->nbr_of_meal = -1;
 	while (av[i])
 	{
-		if (is_all_digits(av[i]) || put_in_struct(av[i], sim_data, i))
+		if (is_all_digits(av[i]))
+			return (EXIT_FAILURE);
+		if (put_in_struct(av[i], sim_data, i))
 			return (EXIT_FAILURE);
 		++i;
 	}
@@ -55,7 +61,10 @@ int	init_sim_data(char **av, t_sim_data **sim_data)
 {
 	*sim_data = malloc(sizeof(t_sim_data));
 	if (!*sim_data)
+	{
+		write(2, "error: memory allocation failed\n", 33);
 		return (EXIT_FAILURE);
+	}
 	if (fill_simulation_data(av, *sim_data))
 	{
 		free(*sim_data);
--- philo/src/parsing/is_all_digit.c
+++ philo/src/parsing/is_all_digit.c
@@ -6,11 +6,13 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 23:17:28 by enchevri          #+#    #+#             */
-/*   Updated: 2025/08/24 00:36:05 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/08/24 01:09:58 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "style.h"
 #include "utils.h"
+#include <unistd.h>
 
 int	is_all_digits(const char *str)
 {
@@ -18,11 +20,17 @@ int	is_all_digits(const char *str)
 
 	i = 0;
 	if (!str[i])
+	{
+		write(2, "Error : empty arg\n", 19);
 		return (EXIT_FAILURE);
+	}
 	while (str[i])
 	{
 		if (str[i] > '9' || str[i] < '0')
+		{
+			write(2, "error: invalid character in argument\n", 38);
 			return (EXIT_FAILURE);
+		}
 		++i;
 	}
 	return (EXIT_SUCCESS);
