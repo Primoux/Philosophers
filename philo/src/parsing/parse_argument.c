fe2cf437f9b5207e414803234208bbd88c8ae8ff
Enzo <enzochevrier@icloud.com>
2025-08-24 15:04:38 +0200

    Improve error message formatting in parse_argument.c

--- philo/src/parsing/parse_argument.c
+++ philo/src/parsing/parse_argument.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_argument.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: enzo <enzo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 21:53:55 by enchevri          #+#    #+#             */
-/*   Updated: 2025/08/24 01:12:25 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/08/24 14:59:55 by enzo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int	check_nbr_arg(int ac)
 	if (ac != 5 && ac != 6)
 	{
 		write(2, "Error: Invalid number of arguments\n", 36);
-		write(2, "Usage: ./philo number_of_philosophers time_to_die ", 51);
-		write(2, "time_to_eat time_to_sleep [number_of_meals]\n", 45);
+		write(2, "./philo <number_of_philosophers> <time_to_die> ", 48);
+		write(2, "<time_to_eat <time_to_sleep> [number_of_meals]\n", 48);
 		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
